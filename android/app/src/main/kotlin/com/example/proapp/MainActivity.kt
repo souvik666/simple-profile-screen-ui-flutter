d0a6c21e2df52f2e6e6677a291952c8bc56d9302
souvik666 <peacedynasty0@gmail.com>
2020-09-03 20:41:07 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/proapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Profile Page",
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
+        backgroundColor: Colors.blue[50],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('images/weed.png'),
+                ),
+                Text(
+                  'Hello World',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 25,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        'Phone Number',
+                        style:
+                            TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+                      ),
+                    )),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.person,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'Name',
+                      style: TextStyle(fontSize: 20.0, fontFamily: 'Neucha'),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
